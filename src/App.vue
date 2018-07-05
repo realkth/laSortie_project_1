0566fe7c1b45b6e3aaf93d3f6ed34fa93fa43e7c
realkth <miamirocky@gmail.com>
2018-07-05 23:01:41 +0900

    다음 지도 불러오기 및 Sass 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,38 @@
 
 <template lang="html">
   <div id="app">
-    <h1>Hello Parcel 📦 🚀</h1>
+    <div id="map" style="width:500px;height:400px;">
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'app'
-}
+  name: "app"
+};
+
 </script>
 
-<style lang="css">
-  html,
-  body {
-    height: 100%;
-  }
+<style lang="scss">
+html,
+body {
+  height: 100%;
+}
 
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial,
-    sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  }
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+}
 
-  #app {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-  }
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
 
-  h1 {
-    font-weight: 300;
-  }
+h1 {
+  font-weight: 300;
+}
 </style>
